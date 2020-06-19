2637675cd98a1a0152a0ae5d43e4ba7bf17070b3
Lizzyfemme <lizzy_swann@hotmail.com>
2020-06-19 07:59:56 -0700

    app.ts broken up into smaller files

---
+++ src/components/project-items.ts
@@ -0,0 +1,42 @@
+/// <reference path="./base-component.ts" />
+
+namespace app {
+  //ProjectItemClass
+  export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
+    implements Draggable {
+    private project: Project;
+
+    get persons() {
+      if (this.project.people === 1) {
+        return "1 person";
+      } else {
+        return `${this.project.people} persons`;
+      }
+    }
+
+    constructor(hostId: string, project: Project) {
+      super("single-project", hostId, false, project.id);
+      this.project = project;
+      this.configure();
+      this.renderContent();
+    }
+    @autobind
+    dragStartHandler(event: DragEvent) {
+      event.dataTransfer!.setData("text/plain", this.project.id);
+      event.dataTransfer!.effectAllowed = "move";
+    }
+
+    dragEndHandler(_: DragEvent) {
+      console.log("dragEnd");
+    }
+    configure() {
+      this.element.addEventListener("dragstart", this.dragStartHandler);
+    }
+    renderContent() {
+      this.element.querySelector("h2")!.textContent = this.project.title;
+      this.element.querySelector("h3")!.textContent =
+        this.persons + " assigned";
+      this.element.querySelector("p")!.textContent = this.project.description;
+    }
+  }
+}
